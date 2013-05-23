dff780ab5979cfff2499c8285f2935c3bfcf7c3e
VladimirSedykh <SWA88@mail.ru>
2013-05-23 23:04:40 +0400

    MOdified user/id post display.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,21 @@
 class PostsController < ApplicationController
 
+	# GET /posts
 	def index
 		@posts = Post.all
 	end
 
+	# GET /posts/1
+  def show
+    @post = Post.find(params[:id])
+  end
+
+	# GET /posts/new
 	def new
 		@post = Post.new
 	end
 
+	# POST /posts
 	def create
 		@post = current_user.posts.build(params[:post])
 		if @post.save
@@ -17,6 +25,7 @@ class PostsController < ApplicationController
 		end
 	end
 
+	# DELETE /posts/1
 	def destroy
     @post = Post.find(params[:id])
     @post.destroy
